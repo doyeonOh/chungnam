bec5d02497ac8df03c802712f9b3eae108566f34
doyeonoh <odyody12@ody12.cafe24.com>
2015-02-05 09:40:52 +0900

    update contact and aboutus ui

--- app/views/aboutus/aboutus.js
+++ app/views/aboutus/aboutus.js
@@ -5,4 +5,10 @@ var introControllers = angular.module('chungnamApp.aboutus', []);
 introControllers.controller('AboutusCtrl', ['$scope', '$http',
         function ($scope, $http) {
                 $scope.subject = '주변환경';
+		$scope.about = {
+			"one": "드라이버 방향을 진단할 수 있는 비거리 200야드",
+			"two": "천연잔디 위에서 치는 생동감",
+			"three": "골프연습장에서 10분거리에 있는 골프장 ",
+			"four": "5곳(해비치ᆞ더클래식ᆞ부영ᆞ스프링데일ᆞ우리들)이 있습니다"
+		};
         }]);
--- app/views/contact/contact.js
+++ app/views/contact/contact.js
@@ -6,6 +6,8 @@ contactControllers.controller('ContactCtrl', ['$scope', '$http',
         function ($scope, $http) {
                 $scope.subject = '연락처';
 		
+		$scope.address_old = '구)제주특별자치도 서귀포시 남원읍 한남리 215-0번지 청남골프연습장';
+		$scope.address_new = '신)제주특별자치도 서귀포시 남원읍 중산간동로 6286 청남골프연습장';
 		//var container = document.getElementById('map');
 		//var options = {
 		//	center: new daum.maps.LatLng(33.450701, 126.570667),
