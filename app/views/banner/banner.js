743ca285751a0dab500dbee466c4663e8611af53
doyeonoh <odyody12@ody12.cafe24.com>
2015-02-05 18:17:10 +0900

    뷰 naming 및 위치 재조정

--- app/views/banner/banner.js
+++ app/views/banner/banner.js
@@ -4,5 +4,6 @@ var bannerControllers = angular.module('chungnamApp.banner', []);
 
 bannerControllers.controller('BannerCtrl', ['$scope', '$http', 
 	function ($scope, $http) {
-		$scope.name = 'banner';
+		$scope.subject = '청남골프연습장에 오신 것을 환영합니다';
+		$scope.content = '본 사이트는 HTML5 에 최적화 되어 있습니다';
 	}]);
